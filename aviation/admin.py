0a99ae23be30116ff519c730b8feb2932e29c13d
Abai <abai.org@gmail.com>
2022-06-15 14:55:48 +0600

    Admin.py - list_display

--- aviation/admin.py
+++ aviation/admin.py
@@ -1,2 +1,8 @@
 from django.contrib import admin
+from .models import Aircraft
 
+
+@admin.register(Aircraft)
+class AircraftAdmin(admin.ModelAdmin):
+    list_display = ('aircraft', 'status', 'priority', 'type', 'errors_count', 'info_count',)
+    search_fields = ('aircraft',)
