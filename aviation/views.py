bf57bf3284a63cbe2792679c628fcf5eab401c1e
Abai <abai.org@gmail.com>
2022-06-15 14:59:03 +0600

    views  handling upload and bulk aircraft creation

---
+++ aviation/views.py
@@ -0,0 +1,48 @@
+import csv
+from django.shortcuts import render
+from django.core.files.base import ContentFile
+from django.core.files.storage import FileSystemStorage
+from .models import Aircraft
+
+fs = FileSystemStorage(location='tmp/')
+
+
+def index(request):
+    if request.method == 'POST':
+        file = request.FILES.get('dataFile')
+        content = file.read()
+        file_content = ContentFile(content)
+        file_name = fs.save(
+            "_tmp.csv", file_content
+        )
+        tmp_file = fs.path(file_name)
+
+        csv_file = open(tmp_file)
+        reader = csv.reader(csv_file)
+        next(reader)
+
+        product_list = []
+        for id_, row in enumerate(reader):
+            (
+                priority,
+                type,
+                aircraft,
+                status,
+                errors_count,
+                info_count,
+            ) = row
+            product_list.append(
+                Aircraft(
+                    priority=priority,
+                    type=type,
+                    aircraft=aircraft,
+                    status=status,
+                    errors_count=errors_count,
+                    info_count=info_count,
+                )
+            )
+
+        Aircraft.objects.bulk_create(product_list)
+        fs.delete(tmp_file)
+
+    return render(request, 'index.html', {})
