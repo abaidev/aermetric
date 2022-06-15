8811f696fa02d5e3ad9bb389c3565c7c44e55f12
Abai <abai.org@gmail.com>
2022-06-15 14:54:00 +0600

    Aircraft model

--- aviation/models.py
+++ aviation/models.py
@@ -1,1 +1,39 @@
 from django.db import models
+
+CHOICES = {
+    'priority': (
+        ('Easy', 'Easy'),
+        ('Hard', 'Hard'),
+        ('Usual', 'Usual')
+    ),
+    'type': (
+        ('PreLegend', 'PreLegend'),
+        ('Legend', 'Legend'),
+        ('Repeat Legend', 'Repeat Legend'),
+        ('Lower A', 'Lower A'),
+        ('Lower B', 'Lower B'),
+        ('Upper A', 'Upper A'),
+        ('Warning', 'Warning'),
+        ('Paired A', 'Paired A'),
+        ('Paired B', 'Paired B'),
+    ),
+    'status': (
+        ('Finished', 'Finished'),
+        ('In progress', 'In progress'),
+        ('Kickback', 'Kickback'),
+        ('Started', 'Started'),
+        ('Suspend', 'Suspend')
+    )
+}
+
+
+class Aircraft(models.Model):
+    priority = models.CharField(max_length=15, choices=CHOICES['priority'])
+    type = models.CharField(max_length=50, choices=CHOICES['type'])
+    aircraft = models.CharField(max_length=150)  # что если назвать craft_model ?
+    status = models.CharField(max_length=50, choices=CHOICES['status'])
+    errors_count = models.IntegerField(default=0)
+    info_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.aircraft
