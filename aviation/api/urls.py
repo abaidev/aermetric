46816dd69c037e17b8c048ef0819e429e28657fd
Abai <abai.org@gmail.com>
2022-06-16 09:24:43 +0600

    api view. Displaying all required statistical data with one endpoint

--- aviation/api/urls.py
+++ aviation/api/urls.py
@@ -4,7 +4,5 @@ from . import views
 app_name = 'aviation-api'
 
 urlpatterns = [
-    path('stats/aircraft/<str:craft_model>/', views.aircraft_stats, name='aircraft_stats'),
-    path('stats/status/<str:status>/', views.status_stats, name='status_stats'),
-    # path('stats/type/<str:type>/', views.type_stats, name='type_stats'),
+    path('stats/aircraft/', views.aircraft_stats, name='aircraft_stats'),
 ]
--- aviation/api/views.py
+++ aviation/api/views.py
@@ -2,49 +2,44 @@ from django.apps import apps
 from django.db.models import Sum
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from aviation.models import CHOICES
 
 Aircraft = apps.get_model("aviation", "Aircraft")
 
 
 @api_view(http_method_names=['GET'])
-def aircraft_stats(request, craft_model):
-    qs = Aircraft.objects.filter(aircraft=craft_model)
-    response_data = {
-        "aircraft": craft_model,
-        "status": None,
-        "type": None,
-        "info_count": qs.aggregate(Sum("info_count"))["info_count__sum"],
-        "errors_count": qs.aggregate(Sum("errors_count"))["errors_count__sum"],
-        "pre_legend": qs.filter(type="PreLegend").count(),
-        "warning": qs.filter(type="Warning").count(),
-        "paired_b": qs.filter(type="Paired B").count(),
-        "legend": qs.filter(type="Legend").count(),
-        "lower_b": qs.filter(type="Lower B").count(),
-        "repeat_legend": qs.filter(type="Repeat Legend").count(),
-        "upper_a": qs.filter(type="Upper A").count(),
-        "lower_a": qs.filter(type="Lower A").count(),
-        "paired_a": qs.filter(type="Paired A").count(),
-    }
-    return Response(response_data)
+def aircraft_stats(request):
+    def data_form(qs, aircraft=None, status=None, type=None):
+        return {
+            "aircraft": aircraft,
+            "status": status,
+            "type": type,
+            "info_count": qs.aggregate(Sum("info_count"))["info_count__sum"],
+            "errors_count": qs.aggregate(Sum("errors_count"))["errors_count__sum"],
+            "pre_legend": qs.filter(type="PreLegend").count(),
+            "warning": qs.filter(type="Warning").count(),
+            "paired_b": qs.filter(type="Paired B").count(),
+            "legend": qs.filter(type="Legend").count(),
+            "lower_b": qs.filter(type="Lower B").count(),
+            "repeat_legend": qs.filter(type="Repeat Legend").count(),
+            "upper_a": qs.filter(type="Upper A").count(),
+            "lower_a": qs.filter(type="Lower A").count(),
+            "paired_a": qs.filter(type="Paired A").count(),
+        }
 
+    res_data = []
+    aircraft_models = Aircraft.objects.values_list('aircraft', flat=True).distinct()
 
-@api_view(http_method_names=['GET'])
-def status_stats(request, status):
-    qs = Aircraft.objects.filter(status=status)
-    response_data = {
-        "aircraft": None,
-        "status": status,
-        "type": None,
-        "info_count": qs.aggregate(Sum("info_count"))["info_count__sum"],
-        "errors_count": qs.aggregate(Sum("errors_count"))["errors_count__sum"],
-        "pre_legend": qs.filter(type="PreLegend").count(),
-        "warning": qs.filter(type="Warning").count(),
-        "paired_b": qs.filter(type="Paired B").count(),
-        "legend": qs.filter(type="Legend").count(),
-        "lower_b": qs.filter(type="Lower B").count(),
-        "repeat_legend": qs.filter(type="Repeat Legend").count(),
-        "upper_a": qs.filter(type="Upper A").count(),
-        "lower_a": qs.filter(type="Lower A").count(),
-        "paired_a": qs.filter(type="Paired A").count(),
-    }
-    return Response(response_data)
+    for aircraft in aircraft_models:
+        qs = Aircraft.objects.filter(aircraft=aircraft)
+        res_data.append(data_form(qs, aircraft=aircraft))
+
+    for status, _ in CHOICES['status']:
+        qs = Aircraft.objects.filter(status=status)
+        res_data.append(data_form(qs, status=status))
+
+    for ac_type, _ in CHOICES['type']:
+        qs = Aircraft.objects.filter(type=ac_type)
+        res_data.append(data_form(qs, type=ac_type))
+
+    return Response(res_data)
